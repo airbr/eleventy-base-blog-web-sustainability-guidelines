fa8a07d4a3d14e0beab201bbf831c2adfe0c982f
Morgan Murrah <morgan@morganwebdev.com>
2023-10-23 18:47:50 -0700

    Update

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Commentary on the Web Sustainability Guidelines JSON",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am enjoying testing out websites",
+	description: "Information about Web Sustainability",
 	author: {
 		name: "Morgan Murrah",
 		email: "morgan@morganwebdev.com",
